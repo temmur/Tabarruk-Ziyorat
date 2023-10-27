1a1b2d44e5cbf6a46b9ce847042fb67ea9254b0a
temmur <143727496+temmur@users.noreply.github.com>
2023-10-27 19:25:49 +0500

    created logic for search input for header

--- src/components/CSearch.vue
+++ src/components/CSearch.vue
@@ -6,11 +6,10 @@
       <div v-if="show && search.length > 2">
         <ul class="rounded-lg border-1 border-white/20 bg-white/10 backdrop-blur-xl pl-4 mt-3 w-[600px] absolute">
           <li
-            v-for="(country, idx) in countries"
-            :key="idx"
+          v-for="object in filterObjects" :key="object.country"
             class="p-4 pl-0 border-b border-b-white/[0.08] last:border-none"
           >
-            <p class="text-white" >{{ country.country }}</p>
+          {{ object.name }}
           </li>
         </ul>
       </div>
@@ -19,14 +18,22 @@
   <script setup lang="ts">
   import CInput from './Form/CInput.vue'
   import { countries } from '@/constants/countrySlide.js'
-  import { ref, reactive } from 'vue'
+  import { ref, reactive, computed } from 'vue'
   const search = ref('')
   const show = ref(false)
 
+  const objectList = ref([
+      { name: 'Object 1', value: 10 },
+      { name: 'Object 2', value: 20 },
+      { name: 'Object 3', value: 30 },
+      
+    ]);
   const filterObjects = computed(()=>{
-    
-    return 
+      return objectList.value.filter((object)=>
+      object.name.toLowerCase().includes(search.value.toLowerCase())
+      );
   })
 
+  
   
   </script>
